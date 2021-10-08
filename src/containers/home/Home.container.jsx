ba510ff31471b0d9a4bd720f4356b2201cddfd9a
Rob Wilson <robwilson101@gmail.com>
2021-10-08 16:18:58 +0100

    optimised

--- src/containers/home/Home.container.jsx
+++ src/containers/home/Home.container.jsx
@@ -1,32 +1,69 @@
 import * as React from "react";
-import * as styles from './Home.module.css'
+import { StaticImage } from "gatsby-plugin-image";
 
-import heroCar from '../../images/hero.jpg'
-import car1 from '../../images/car1.jpg'
-import car2 from '../../images/car2.jpg'
-import car3 from '../../images/car3.jpg'
+import * as styles from "./Home.module.css";
+
+const heroCar = "../../images/hero.jpg";
+const car1 = "../../images/car1.jpg";
+const car2 = "../../images/car2.jpg";
+const car3 = "../../images/car3.jpg";
 
 const HomeContainer = () => (
   <main className={styles.main}>
-    <title>Gatsby | Unoptimised Images</title>
+    <title>Gatsby | Optimised Images</title>
 
-    <h1 className={styles.heading}>
-      Gatsby Unoptimised Images
-    </h1>
+    <h1 className={styles.heading}>Gatsby Optimised Images</h1>
 
     <section className={styles.heroImage}>
-        <img src={heroCar} alt="Hero" height={800} />
+      <StaticImage
+        src={heroCar}
+        alt="Hero Car"
+        placeholder="blurred"
+        layout="constrained"
+        width={1200}
+        height={800}
+        loading="eager"
+        formats={["auto", "webp", "avif"]}
+        quality={80}
+      />
     </section>
 
     <section className={styles.otherImages}>
-        {[car1, car2, car3].map(
-            (car, idx) => <img src={car} alt={`Car ${idx + 1}`} height={300} />
-        )}
-
+        <StaticImage
+          src={car1}
+          alt="Car 1"
+          placeholder="blurred"
+          layout="constrained"
+          width={450}
+          height={300}
+          loading="lazy"
+          formats={["auto", "webp", "avif"]}
+          quality={50}
+        />
+        <StaticImage
+          src={car2}
+          alt="Car 2"
+          placeholder="blurred"
+          layout="constrained"
+          width={450}
+          height={300}
+          loading="lazy"
+          formats={["auto", "webp", "avif"]}
+          quality={50}
+        />
+        <StaticImage
+          src={car3}
+          alt="Car 3"
+          placeholder="blurred"
+          layout="constrained"
+          width={450}
+          height={300}
+          loading="lazy"
+          formats={["auto", "webp", "avif"]}
+          quality={50}
+        />
     </section>
-
-
   </main>
-)
+);
 
 export default HomeContainer;
